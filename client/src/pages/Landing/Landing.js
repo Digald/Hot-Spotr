7fff4e570c0afaeba5a8add91c55dfd9cbfee5a1
Mark Alaniz <markalaniz000@gmail.com>
2018-04-04 00:00:15 -0500

    created file structure for components and pages

---
+++ client/src/pages/Landing/Landing.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <section className="hero">
+            <div className="hero-body">
+              <div className="container">
+                <h1 className="title">
+                  Hero title
+                </h1>
+                <h2 className="subtitle">
+                  Hero subtitle
+                </h2>
+              </div>
+            </div>
+          </section>
+        )
+    }
+}
+
+export default Landing;
