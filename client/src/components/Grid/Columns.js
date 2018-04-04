de6b02a39c40ed6d5e56026bf1431257fc552273
Digald <nosracer_gt@sbcglobal.net>
2018-04-04 18:32:11 -0500

    added div wrapper to column and need to adjust to be mobile responsive

--- client/src/components/Grid/Columns.js
+++ client/src/components/Grid/Columns.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from "react";
 
-export const Columns = (props) => (
-    <div className={`columns banner ${props.style}`}>
-        {props.children}
-    </div>
+export const Columns = props => (
+  <div className={`columns banner ${props.style}`}>{props.children}</div>
 );
--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -9,31 +9,33 @@ class Landing extends Component {
   render() {
     return (
       <Container size="is-fluid">
-        <Columns>
-          <Column>
-            <Columns>
-              <Column />
-              <Column size="is-narrow">
-                <Logo />
-              </Column>
-              <Column>
-                <h1>Buisnes App</h1>
-                <p>So that you don't have too.</p>
-                <Columns>
-                  <Column>
-                    <GenericBtn>Sign Up</GenericBtn>
-                  </Column>
-                  <Column>
-                    <GenericBtn>Sign In</GenericBtn>
-                  </Column>
-                </Columns>
-              </Column>
-            </Columns>
-          </Column>
-          <Column>
-            <IphoneMockUp />
-          </Column>
-        </Columns>
+        <div className="wrapper">
+          <Columns>
+            <Column>
+              <Columns>
+                <Column />
+                <Column size="is-narrow">
+                  <Logo />
+                </Column>
+                <Column>
+                  <h1>Buisnes App</h1>
+                  <p>So that you don't have too.</p>
+                  <Columns>
+                    <Column>
+                      <GenericBtn>Sign Up</GenericBtn>
+                    </Column>
+                    <Column>
+                      <GenericBtn>Sign In</GenericBtn>
+                    </Column>
+                  </Columns>
+                </Column>
+              </Columns>
+            </Column>
+            <Column>
+              <IphoneMockUp />
+            </Column>
+          </Columns>
+        </div>
       </Container>
     );
   }
