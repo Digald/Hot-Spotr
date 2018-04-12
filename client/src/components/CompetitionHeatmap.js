163b33c77b741370429a36787a4bb95db2e66ac7
William Blake <willblakebooking@gmail.com>
2018-04-11 20:06:39 -0500

    initial commit

---
+++ client/src/components/CompetitionHeatmap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class CompetitionHeatmap extends React.Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default CompetitionHeatmap;
---
+++ client/src/components/TargetDemographics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TargetDemographics extends React.Component {
+    render() {
+        return(
+
+        )
+    }
+}
+
+export default TargetDemographics;
