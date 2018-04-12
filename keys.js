e8dc67bc2bf366272e1eaf545c12586e285ce41c
Digald <nosracer_gt@sbcglobal.net>
2018-04-11 21:18:39 -0500

    added auth0 routes and need to add users to own database

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.auth0 = {
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENTID,
+    clientSecret: process.env.CLIENTSECRET,
+    callbackURL: process.env.CALLBACKURL
+}
--- server.js
+++ server.js
@@ -1,8 +1,39 @@
+require("dotenv").config();
+const keys = require("./keys.js");
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+const router = require('./routes');
+
+
+// auth0 work----------------------------------
+// Configure Passport to use Auth0
+const strategy = new Auth0Strategy(
+  {
+    domain: keys.auth0.domain,
+    clientID: keys.auth0.clientID,
+    clientSecret: keys.auth0.clientSecret,
+    callbackURL: keys.auth0.callbackURL
+  },
+  (accessToken, refreshToken, extraParams, profile, done) => {
+    return done(null, profile);
+  }
+);
+passport.use(strategy);
+// This can be used to keep a smaller payload
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+// ...
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Import sequelize models
 const db = require("./models");
@@ -14,14 +45,14 @@ app.use(express.static("client/build"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Send every request to the React app
+app.use('/',router); 
 // Define any API routes before this runs
-app.get("*", function (req, res) {
+app.use("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-db.sequelize.sync({ force: false }).then(function () {
-  app.listen(PORT, function () {
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
     console.log(`🌎 ==> Server now on port ${PORT}!`);
   });
 });
