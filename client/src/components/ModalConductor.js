036970ad19ee8527d549aef475b655a1c1240378
William Blake <willblakebooking@gmail.com>
2018-04-13 14:06:48 -0500

    actions file and reducers file

--- client/src/components/ModalConductor.js
+++ client/src/components/ModalConductor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import * as actions from '../actions/actionCreators.js';
+import * as actions from '../actions/actionCreator.js';
 import TargetIndustryForm from './TargetIndustryForm.js';
 import TargetLocationForm from './TargetLocationForm';
 import TargetDemographicsForm from './TargetDemographicsForm';
