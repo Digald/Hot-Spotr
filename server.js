ac190c9bc525de8ed92509133f8d5435fc76cd39
Mark Alaniz <markalaniz000@gmail.com>
2018-04-12 23:02:04 -0500

    added local passport.js config, need to handle routes properly now

--- server.js
+++ server.js
@@ -1,56 +1,46 @@
 require("dotenv").config();
 const keys = require("./keys.js");
 const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require("path");
 const passport = require("passport");
-const Auth0Strategy = require("passport-auth0");
-const router = require('./routes');
+const flash = require('connect-flash');
 
-
-// auth0 work----------------------------------
-// Configure Passport to use Auth0
-const strategy = new Auth0Strategy(
-  {
-    domain: keys.auth0.domain,
-    clientID: keys.auth0.clientID,
-    clientSecret: keys.auth0.clientSecret,
-    callbackURL: keys.auth0.callbackURL
-  },
-  (accessToken, refreshToken, extraParams, profile, done) => {
-    return done(null, profile);
-  }
-);
-passport.use(strategy);
-// This can be used to keep a smaller payload
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-// ...
-app.use(passport.initialize());
-app.use(passport.session());
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const session = require('express-session');
 
 // Import sequelize models
 const db = require("./models");
 
-// Serve up static assets (usually on heroku)
+// pass passport for configuration
+require('./config/passport')(passport);
+
+// Serve up static assets
 app.use(express.static("client/build"));
 
-// Set up body parser from documentation
+// Set up express app
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/',router); 
+// required for passport
+app.use(session({ secret: 'zomaareenstukjetekstDatjenietzomaarbedenkt' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 // Define any API routes before this runs
 app.use("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Run server and sync database
 db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
     console.log(`🌎 ==> Server now on port ${PORT}!`);
