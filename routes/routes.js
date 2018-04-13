605bc472f78a55a99964da34e41a2914a3e482d8
Mark Alaniz <markalaniz000@gmail.com>
2018-04-12 23:24:14 -0500

    trying to get user in database with postman

--- routes/routes.js
+++ routes/routes.js
@@ -44,8 +44,8 @@ module.exports = function(app, passport) {
   app.post(
     "/signup",
     passport.authenticate("local-signup", {
-      successRedirect: "/profile", // redirect to the secure profile section
-      failureRedirect: "/signup", // redirect back to the signup page if there is an error
+      successRedirect: "/", // redirect to the secure profile section
+      failureRedirect: "/", // redirect back to the signup page if there is an error
       failureFlash: true // allow flash messages
     })
   );
