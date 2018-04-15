f62025835b0fc93118eabb26539d6e0da64e3e1e
William Blake <willblakebooking@gmail.com>
2018-04-15 16:33:16 -0500

    store pipeline working I think

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import App from '../App';
+import { Provider } from 'react-redux';
+import store, { history } from '../store';
+import App from './App';
 import Landing from '../pages/Landing';
 import Dashboard from '../pages/Dashboard';
 import ModalConductor from './ModalConductor';
 
 const Router = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/mydash' component={Dashboard} />
-            <Route path='/store/:profile-section' component={ModalConductor} />
-            <Route path='/store/:storeId' component={App} />
-        </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={Landing} />
+                <Route exact path='/mydash' component={Dashboard} />
+                <Route path='/store/:profile-section' component={ModalConductor} />
+                <Route path='/store/:storeId' component={App} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
 )
 
 export default Router;
