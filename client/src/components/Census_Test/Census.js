937e2ded4d5df2d448d399e6d8911e585e2e96b6
Pete <petegarcia54@sbcglobal.net>
2018-04-16 21:15:29 -0500

    Got the Census API to work

--- client/src/components/Census_Test/Census.js
+++ client/src/components/Census_Test/Census.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export default  {
   getCensus: function() {
-    return axios.get("https://api.census.gov/data/2016/acs/acs1?get=NAME,C27010_017E&for=county:209,453,491&in=state:48&key=a8b3b659fe228e5f51462803ee92e1868886a493");
+    return axios.get("https://api.census.gov/data/2016/acs/acs1?get=NAME,C27010_017E,B01001_027E&for=county:209,453,491&in=state:48&key=a8b3b659fe228e5f51462803ee92e1868886a493");
   }
 };
