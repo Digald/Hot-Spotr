aaadffc05da4f25b6c77b9ed84c2c04f9321e718
William Blake <willblakebooking@gmail.com>
2018-04-17 23:55:34 -0500

    resolving more

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Footer = props => (
-        <p class="footer-block">
+        <p className="footer-block">
             <span>
                 <a href='https://github.com/Project3-WillMarkPete/Project3' target="_blank" rel="noopener noreferrer">
-                    <i class="fab fa-github-alt"></i>
+                    <i className="fab fa-github-alt"></i>
                 </a>
             </span> Created By: Pete Garcia, Mark Alaniz, &amp; Will Blake 2018 &copy;
         </p>
