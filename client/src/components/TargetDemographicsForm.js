fa30259c7ca2bf1f37ca89a3aabd9db48f086830
William Blake <willblakebooking@gmail.com>
2018-04-17 23:52:41 -0500

    removed most warnings

--- client/src/components/TargetDemographicsForm.js
+++ client/src/components/TargetDemographicsForm.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import ModalWrapper from './ModalWrapper';
+// import ModalWrapper from './ModalWrapper';
 
 const TargetDemographicsForm = props => {
-    const updateDemographic = demographic => {
-        props.hideModal();
-        props.updateDemographic(demographic);
-    };
-
     return (
-        <ModalWrapper
-            {...props}
-            title="UserProfile"
-            width={400}
-            showOk={false}
-        >
-            <p>What is your target demographic?</p>
-            <input />
-            <button onClick={() => updateDemographic()}>Update</button>
-        </ModalWrapper>
+        <div className="modal">
+            <div className="modal-background"></div>
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">Modal title</p>
+                    <button className="delete" aria-label="close"></button>
+                </header>
+                <section className="modal-card-body">
+                    {/* <!-- Content ... --> */}
+    </section>
+                <footer className="modal-card-foot">
+                    <button className="button is-success">Save changes</button>
+                    <button className="button">Cancel</button>
+                </footer>
+            </div>
+        </div>
     )
 }
 
